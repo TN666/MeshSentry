b58ff26ae5901d96d3d3bf0261c3fef33d1fe6e7
TN666 <t841222@gmail.com>
2025-09-03 22:14:57 +0800

    feat: support multi nodes

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ from influxdb_client import InfluxDBClient, Point
 from influxdb_client.client.write_api import SYNCHRONOUS
 import os
 from dotenv import load_dotenv
+import threading
+
 
 def write_to_influxdb(url, token, org, bucket, data):
     client = InfluxDBClient(url=url, token=token, org=org)
@@ -15,6 +17,7 @@ def write_to_influxdb(url, token, org, bucket, data):
         .tag("id", data["user"]["id"])
         .tag("shortName", data["user"]["shortName"])
         .tag("longName", data["user"]["longName"])
+        .tag("fw_version", data["fw_version"])
         .tag("macaddr", data["user"]["macaddr"])
         .field("hwModel", data["user"]["hwModel"])
         .field("batteryLevel", data["deviceMetrics"]["batteryLevel"])
@@ -43,16 +46,18 @@ def write_to_influxdb(url, token, org, bucket, data):
 
     try:
         write_api.write(bucket=bucket, org=org, record=point)
+        print("Data written to InfluxDB successfully.")
     except Exception as e:
         print(f"Error writing to InfluxDB: {e}")
 
     client.close()
 
 
-def get_node_data(node):
+def get_node_data(node, fw_version):
     # Extract relevant data from node object
     data = {
         "num": node.get("num"),
+        "fw_version": fw_version,
         "user": {
             "id": node.get("user", {}).get("id"),
             "longName": node.get("user", {}).get("longName"),
@@ -73,7 +78,9 @@ def get_node_data(node):
         "deviceMetrics": {
             "batteryLevel": node.get("deviceMetrics", {}).get("batteryLevel"),
             "voltage": node.get("deviceMetrics", {}).get("voltage"),
-            "channelUtilization": node.get("deviceMetrics", {}).get("channelUtilization"),
+            "channelUtilization": node.get("deviceMetrics", {}).get(
+                "channelUtilization"
+            ),
             "airUtilTx": node.get("deviceMetrics", {}).get("airUtilTx"),
             "uptimeSeconds": node.get("deviceMetrics", {}).get("uptimeSeconds"),
         },
@@ -92,28 +99,54 @@ def get_node_data(node):
             "numTxRelayCanceled": node.get("localStats", {}).get("numTxRelayCanceled"),
             "heapTotalBytes": node.get("localStats", {}).get("heapTotalBytes"),
             "heapFreeBytes": node.get("localStats", {}).get("heapFreeBytes"),
-        }
+        },
     }
     return data
 
-interface = meshtastic.serial_interface.SerialInterface()
-load_dotenv('.env')
+
+def monitor_port(port, url, token, org, bucket, time_interval):
+    try:
+        interface = meshtastic.serial_interface.SerialInterface(devPath=port)
+        while True:
+            try:
+                fw_version = interface.metadata.firmware_version
+                info = interface.getMyNodeInfo()
+                print(f"Port {port} - node info: {info}")
+                node_data = get_node_data(info, fw_version)
+                print(f"Port {port} - Writing to InfluxDB:", node_data)
+                write_to_influxdb(url, token, org, bucket, node_data)
+                time.sleep(int(time_interval))
+            except Exception as e:
+                print(f"Error monitoring port {port}: {e}")
+                break
+    except Exception as e:
+        print(f"Failed to connect to port {port}: {e}")
+    finally:
+        if "interface" in locals():
+            interface.close()
+
+
+load_dotenv(".env")
 url = os.getenv("INFLUXDB_URL", "http://localhost:8086")
 token = os.getenv("DOCKER_INFLUXDB_INIT_ADMIN_TOKEN")
 org = os.getenv("DOCKER_INFLUXDB_INIT_ORG", "Meshtastic")
 bucket = os.getenv("DOCKER_INFLUXDB_INIT_BUCKET", "node_info")
 time_interval = os.getenv("TIME_INTERVAL", 30)
 
+ports = meshtastic.util.findPorts(True)
+print("list port", ports)
+
+threads = []
+for port in ports:
+    thread = threading.Thread(
+        target=monitor_port,
+        args=(port, url, token, org, bucket, time_interval)
+    ).start()
+
 try:
     while True:
-        node = interface.getNode("^local")
-        info = interface.getMyNodeInfo()
-        print(f"node info:{info}")
-        node_data = get_node_data(info)
-        print("Writing to InfluxDB:", node_data)
-        write_to_influxdb(url, token, org, bucket, node_data)
-        time.sleep(int(time_interval))
+        time.sleep(1)
 except KeyboardInterrupt:
-    print("Exiting...")
-finally:
-    interface.close()
+    print("Shutting down...")
+    for thread in threads:
+        thread.join(timeout=1)
